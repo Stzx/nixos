ab4935d4d7bfa669b50658cec95bb39d8889f30b
Stzx <silence.m@hotmail.com>
2023-08-06 13:58:23 +0800

    HM: cleanup kde settings

--- home-manager/stzx/kde.nix
+++ home-manager/stzx/kde.nix
@@ -3,118 +3,123 @@
 let
   cfg = config.want;
 
-  cli = { command ? "${pkgs.libsForQt5.kconfig}/bin/kwriteconfig5", rc, args }: (
-    lib.concatMapStringsSep " \n" (arg: "${command} --file ${rc} ${arg}") args
+  command = "${pkgs.libsForQt5.kconfig}/bin/kwriteconfig5";
+
+  directory = "${config.home.homeDirectory}/.config";
+
+  mkCli = rc: args: lib.concatLines (lib.forEach args (arg:
+    "${command} --file ${directory}/${rc} ${arg}"
+  ));
+
+  mkArgs = { groups, kv, onlyKey ? false }: lib.forEach kv (item:
+    let
+      key = if onlyKey then item else builtins.elemAt item 0;
+      value = if onlyKey then "--delete" else builtins.elemAt item 1;
+    in
+    "${lib.concatMapStringsSep " " (g: "--group '${g}'") groups}--key ${key} ${value}"
   );
 
-  args = { g, k, v ? "", del ? false }:
-    let
-      getG = x: lib.concatMapStringsSep " " (i: "--group '${i}'") g;
-      getV = x: if del then "--delete" else v;
-    in
-    "${getG g} --key ${k} ${getV v}";
+  set = groups: kv: mkArgs { inherit groups kv; };
 
-  new = g: k: v: args { inherit g k v; };
-
-  del = g: k: args { inherit g k; del = true; };
+  del = groups: kv: mkArgs { inherit groups kv; onlyKey = true; };
 
   settings = {
 
     "kded5rc" = [
-      (new [ "Module-freespacenotifier" ] "autoload" "false")
-      (new [ "Module-networkstatus" ] "autoload" "false")
-      (new [ "Module-plasma_accentcolor_service" ] "autoload" "false")
-      (new [ "Module-proxyscout" ] "autoload" "false")
-      (new [ "Module-colorcorrectlocationupdater" ] "autoload" "false")
-      (new [ "Module-kded_touchpad" ] "autoload" "false")
-      (new [ "Module-ksysguard" ] "autoload" "false")
+      (set [ "Module-freespacenotifier" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-networkstatus" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-plasma_accentcolor_service" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-proxyscout" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-colorcorrectlocationupdater" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-kded_touchpad" ] [ [ "autoload" "false" ] ])
+      (set [ "Module-ksysguard" ] [ [ "autoload" "false" ] ])
     ];
 
     "plasmashellrc" = [
-      (new [ "PlasmaViews" "Panel 2" ] "panelOpacity" "2")
-      (new [ "PlasmaViews" "Panel 2" ] "panelVisibility" "1")
+      (set [ "PlasmaViews" "Panel 2" ] [
+        [ "panelOpacity" "2" ]
+        [ "panelVisibility" "1" ]
+      ])
     ];
 
     "kwinrc" = [
-      (new [ "Input" ] "TabletMode" "off")
-      (new [ "Plugins" ] "zoomEnabled" "false")
-      (new [ "Plugins" ] "tileseditorEnabled" "false")
-      (new [ "Plugins" ] "blurEnabled" "false")
-      (new [ "Desktops" ] "Name_1" "Main")
+      (set [ "Input" ] [ [ "TabletMode" "off" ] ])
+      (set [ "Plugins" ] [
+        [ "zoomEnabled" "false" ]
+        [ "blurEnabled" "false" ]
+        [ "overviewEnabled" "false" ]
+        [ "tileseditorEnabled" "false" ]
+      ])
+      (set [ "Desktops" ] [
+        [ "Number" "2" ]
+        [ "Name_1" "Tom" ]
+        [ "Name_2" "Jerry" ]
+      ])
     ];
 
     "kdeglobalsrc" = [
-      (new [ "General" ] "UseSystemBell" "true")
-    ] ++ lib.optionals cfg.kitty [
-      (new [ "General" ] "TerminalApplication" "kitty")
-      (new [ "General" ] "TerminalApplicationService" "kitty.desktop")
-    ] ++ lib.optionals cfg.alacritty [
-      (new [ "General" ] "TerminalApplication" "alacritty")
-      (new [ "General" ] "TerminalApplicationService" "Alacritty.desktop")
-    ];
+      (set [ "General" ] [ [ "UseSystemBell" "true" ] ])
+    ] ++ (lib.optional cfg.kitty (set [ "General" ] [
+      [ "TerminalApplication" "kitty" ]
+      [ "TerminalApplicationService" "kitty.desktop" ]
+    ])) ++ (lib.optional cfg.alacritty (set [ "General" ] [
+      [ "TerminalApplication" "alacritty" ]
+      [ "TerminalApplicationService" "Alacritty.desktop" ]
+    ]));
 
     "systemsettingsrc" = [
-      (new [ "systemsettings_sidebar_mode" ] "HighlightNonDefaultSettings" "true")
+      (set [ "systemsettings_sidebar_mode" ] [ [ "HighlightNonDefaultSettings" "true" ] ])
     ];
 
     "powermanagementprofilesrc" = [
-      (del [ "AC" "SuspendSession" ] "idleTime")
-      (del [ "AC" "SuspendSession" ] "suspendThenHibernate")
-      (del [ "AC" "SuspendSession" ] "suspendType")
+      (del [ "AC" "SuspendSession" ] [ "idleTime" "suspendThenHibernate" "suspendType" ])
     ];
 
     "ksplashrc" = [
-      (new [ "KSplash" ] "Engine" "None")
-      (new [ "KSplash" ] "Theme" "None")
+      (set [ "KSplash" ] [
+        [ "Engine" "None" ]
+        [ "Theme" "None" ]
+      ])
     ];
 
     "kactivitymanagerd-statsrc" = [
-      (new [ "Favorites-org.kde.plasma.kickoff.favorites.instance-3-global" ] "ordering" "firefox.desktop")
+      (set [ "Favorites-org.kde.plasma.kickoff.favorites.instance-3-global" ] [ [ "ordering" "firefox.desktop" ] ])
     ];
 
     "kactivitymanagerd-pluginsrc" = [
-      (new [ "Plugin-org.kde.ActivityManager.Resources.Scoring" ] "what-to-remember" "1")
+      (set [ "Plugin-org.kde.ActivityManager.Resources.Scoring" ] [ [ "what-to-remember" "1" ] ])
     ];
 
     "kactivitymanagerdrc" = [
-      (new [ "Plugins" ] "org.kde.ActivityManager.ResourceScoringEnabled" "false")
+      (set [ "Plugins" ] [ [ "org.kde.ActivityManager.ResourceScoringEnabled" "false" ] ])
     ];
 
     "breezerc" = [
-      (new [ "Common" ] "ShadowSize" "ShadowSmall")
-      (new [ "Common" ] "ShadowStrength" "128")
-      (new [ "Style" ] "MenuOpacity" "75")
+      (set [ "Common" ] [
+        [ "ShadowSize" "ShadowSmall" ]
+        [ "ShadowStrength" "128" ]
+      ])
+      (set [ "Style" ] [ [ "MenuOpacity" "75" ] ])
     ];
 
     "dolphinrc" = [
-      (new [ "PlacesPanel" ] "IconSize" "32")
-      (new [ "DetailsMode" ] "IconSize" "32")
-      (new [ "PreviewSettings" ] "Plugins" "svgthumbnail")
+      (set [ "PlacesPanel" ] [ [ "IconSize" "32" ] ])
+      (set [ "DetailsMode" ] [ [ "IconSize" "32" ] ])
+      (set [ "PreviewSettings" ] [ [ "Plugins" "svgthumbnail" ] ])
     ];
 
     "plasma-org.kde.plasma.desktop-appletsrc" = [
-      (new [ "Containments" "2" "Applets" "5" "Configuration" "General" ] "launchers" "preferred://filemanager")
+      (set [ "Containments" "2" "Applets" "5" "Configuration" "General" ] [ [ "launchers" "preferred://filemanager" ] ])
     ];
   };
 
-  commands = (lib.concatLines (
-    lib.mapAttrsToList (rc: args: cli { inherit rc args; }) settings
+  commands = (lib.concatLines (lib.mapAttrsToList
+    (rc: args: mkCli rc (lib.flatten args))
+    settings
   ));
 in
 {
-  home.activation = {
-    kdeSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      XDG_CFG=${config.home.homeDirectory}/.config/
-
-      if [ ! -d $XDG_CFG ]; then
-        mkdir $XDG_CFG
-      fi
-
-      cd $XDG_CFG
-
-      ${commands}
-    '';
-  };
+  home.activation.kdeSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] commands;
 
   programs.zsh.shellAliases.kwin-dbg = "qdbus org.kde.KWin /KWin org.kde.KWin.showDebugConsole";
 }
