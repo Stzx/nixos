0e5f80f216947434c305614b72df18c4fc8ddb05
Stzx <silence.m@hotmail.com>
2023-08-10 18:25:20 +0800

    NixOS: sddm -> greetd

--- modules/nixos/desktop/kde/default.nix
+++ modules/nixos/desktop/kde/default.nix
@@ -15,18 +15,25 @@ in
       xserver = {
         enable = true;
         desktopManager.plasma5.enable = true;
-        displayManager = {
-          sddm.enable = true;
-          defaultSession = "plasmawayland";
+      };
+      greetd = {
+        enable = true;
+        vt = config.services.xserver.tty;
+        settings = {
+          default_session = {
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd startplasma-wayland";
+          };
         };
       };
       pipewire = {
         enable = true;
         alsa.enable = true;
         pulse.enable = true;
-        jack.enable = true;
       };
     };
+
+    # FIXME: https://github.com/NixOS/nixpkgs/issues/248443
+    systemd.user.services.plasma-run-with-systemd.wantedBy = lib.mkForce [ "graphical-session-pre.target" ];
 
     environment.systemPackages = [ pkgs.helvum ];
 
