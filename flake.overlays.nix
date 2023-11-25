c73410fe00ade01bf7dbb7cef207be55baf2cd4a
Stzx <silence.m@hotmail.com>
2023-11-25 10:34:45 +0800

    remove netdata patch

--- flake.overlays.nix
+++ flake.overlays.nix
@@ -1,6 +1,1 @@
-final: prev: {
-  netdata = prev.netdata.overrideAttrs (_: _prev: {
-    # FIXME: https://github.com/NixOS/nixpkgs/pull/249032
-    buildInputs = _prev.buildInputs ++ [ final.json_c ];
-  });
-}
+final: prev: { }
